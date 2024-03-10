915ca257db8affd65c03a100f5562bca5c83da27
JoseDev <joseowen1101@gmail.com>
2024-03-11 03:29:01 +0800

    feat: implemented point transaction page

--- src/pages/dashboard/invoice/InvoiceAdminListPage.js
+++ src/pages/dashboard/invoice/InvoiceAdminListPage.js
@@ -1,21 +1,160 @@
 import { Helmet } from 'react-helmet-async';
+import { useState, useEffect } from 'react';
 // @mui
 import {
-  Box,
+  Button,
+  Card,
+  Table,
+  Divider,
+  TableBody,
   Container,
+  TableContainer,
 } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import moment from 'moment';
 // routes
 import { PATH_DASHBOARD } from '../../../routes/paths';
-
+// components
+import Scrollbar from '../../../components/scrollbar';
 import CustomBreadcrumbs from '../../../components/custom-breadcrumbs';
 import { useSettingsContext } from '../../../components/settings';
-import PreparingContent from '../../../components/preparing';
+import LoadingScreen from '../../../components/loading-screen';
+import {
+  useTable,
+  getComparator,
+  emptyRows,
+  TableNoData,
+  TableEmptyRows,
+  TableHeadCustom,
+  TableSelectedAction,
+  TablePaginationCustom,
+} from '../../../components/table';
+// sections
+import { InvoiceAdminTableToolbar, InvoiceAdminTableRow } from '../../../sections/@dashboard/invoice/list';
+// locales
+import { useLocales } from '../../../locales';
+// api
+import { apiWithPostData, apiWithGetData } from '../../../utils/api';
+// url
+import { balanceHistoryUrl, adminListUrl, transactionUrl } from '../../../utils/urlList';
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  // { id: 'user', label: 'user', align: 'center' },
+  { id: 'permission', label: 'permission', align: 'center' },
+  { id: 'amount', label: 'amount', align: 'center' },
+  { id: 'beforeAmount', label: 'beforeAmount', align: 'center' },
+  { id: 'afterAmount', label: 'afterAmount', align: 'center' },
+  { id: 'style', label: 'style', align: 'center' },
+  { id: 'date', label: 'date', align: 'center' },
+];
 
 // ----------------------------------------------------------------------
 
 export default function InvoiceAdminListPage() {
 
+  const {
+    dense,
+    page,
+    order,
+    orderBy,
+    rowsPerPage,
+    setPage,
+    //
+    selected,
+    onSelectRow,
+    onSelectAllRows,
+    //
+    onSort,
+    onChangeDense,
+    onChangePage,
+    onChangeRowsPerPage,
+  } = useTable();
+  const theme = useTheme();
+  const { translate } = useLocales();
+
   const { themeStretch } = useSettingsContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFirst, setIsFirst] = useState(true);
+  const [tableData, setTableData] = useState([]);
+  const [logData, setLogData] = useState([]);
+  const [filterName, setFilterName] = useState('');
+  const [filterEndDate, setFilterEndDate] = useState(new Date);
+  const [filterStartDate, setFilterStartDate] = useState(new Date);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const dataFiltered = applyFilter({
+    inputData: tableData,
+    comparator: getComparator(order, orderBy),
+    filterName,
+  });
+
+  const denseHeight = dense ? 52 : 72;
+
+  const isFiltered = filterName !== '';
+
+  const isNotFound =
+    (!dataFiltered.length && !!filterName) ||( !tableData.length);
+
+  const handleFilterName = (event) => {
+    setFilterName(event.target.value);
+  };
+
+
+  const handleResetFilter = () => {
+    setFilterName('');
+  };
+
+  const handleClickCreate = () => {
+    createTransaction();
+  };  
+
+  const handleClickSearch = () => {
+    setPage(0);
+    getTransactionList();
+  };  
+
+  
+  const getTransactionList = () => {
+    setIsLoading(true);
+    try {
+      const url = transactionUrl;
+      const headers = {};
+      const data = {};
+      apiWithGetData(url, data, headers).then((response) => {
+        setTableData(response);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  
+  const createTransaction = () => {
+    setIsLoading(true);
+    try {
+      const url = transactionUrl;
+      const headers = {};
+      const data = {};
+      apiWithPostData(url, data, headers).then((response) => {
+        console.log(response);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+
+  useEffect(() => {
+    getTransactionList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isFirst]);
+
 
   return (
     <>
@@ -31,25 +170,123 @@ export default function InvoiceAdminListPage() {
             { name: 'invoices', href: PATH_DASHBOARD.invoice.root },
             { name: 'Admin List' },
           ]}
+          action={
+            <Button
+              variant="contained"
+              sx={{ mt: 4 }}
+              onClick={handleClickCreate}
+            >
+              {/* {translate('create')} */}
+              create
+            </Button>
+          }
         />
-        <Box
-          sx={{
-            mt: 5,
-            width: 1,
-            height: 320,
-            borderRadius: 2,
-            border: (theme) => `dashed 1px ${theme.palette.divider}`,
-          }}>
-          
-          <PreparingContent
-            title="preparing"
-            sx={{
-              '& span.MuiBox-root': { height: 200 },
+        <Card>
+
+          <Divider />
+
+          <InvoiceAdminTableToolbar
+            isFiltered={isFiltered}
+            filterName={filterName}
+            filterEndDate={filterEndDate}
+            filterStartDate={filterStartDate}
+            onFilterName={handleFilterName}
+            onResetFilter={handleResetFilter}
+            onClickSearch={handleClickSearch}
+            onFilterStartDate={(newValue) => {
+              setFilterStartDate(newValue);
+            }}
+            onFilterEndDate={(newValue) => {
+              setFilterEndDate(newValue);
             }}
           />
-        </Box>
+
+          <TableContainer sx={{ position: 'relative', overflow: 'unset' }}>
+            <TableSelectedAction
+              dense={dense}
+              numSelected={selected.length}
+              rowCount={tableData.length}
+              onSelectAllRows={(checked) =>
+                onSelectAllRows(
+                  checked,
+                  tableData.map((row) => row.id)
+                )
+              }
+            />
+
+            <Scrollbar>
+              <Table size={dense ? 'small' : 'medium'} sx={{ minWidth: 800 }}>
+                <TableHeadCustom
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={tableData.length}
+                  numSelected={selected.length}
+                  onSort={onSort}
+                  onSelectAllRows={(checked) =>
+                    onSelectAllRows(
+                      checked,
+                      tableData.map((row) => row.id)
+                    )
+                  }
+                />
+
+                <TableBody>
+                  {dataFiltered
+                    .slice(0, rowsPerPage)
+                    .map((row) => (
+                      <InvoiceAdminTableRow
+                        key={row._id}
+                        row={row}
+                        selected={selected.includes(row._id)}
+                        onSelectRow={() => onSelectRow(row._id)}
+                      />
+                    ))}
+
+                  <TableNoData isNotFound={isNotFound} />
+                </TableBody>
+              </Table>
+            </Scrollbar>
+          </TableContainer>
+
+          <TablePaginationCustom
+            count={totalCount}
+            page={page}
+            rowsPerPage={rowsPerPage}
+            onPageChange={onChangePage}
+            onRowsPerPageChange={onChangeRowsPerPage}
+            //
+            dense={dense}
+            onChangeDense={onChangeDense}
+          />
+        </Card>
       </Container>
+      
+      {(isLoading === true) && <LoadingScreen/>} 
     </>
   );
 }
+
+// ----------------------------------------------------------------------
+
+function applyFilter({ inputData, comparator, filterName, filterStatus }) {
+  const stabilizedThis = inputData.map((el, index) => [el, index]);
+
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+
+  inputData = stabilizedThis.map((el) => el[0]);
+
+  if (filterName) {
+    inputData = inputData.filter(
+      (row) => row.userId?.rolesId?.title.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
+    );
+  }
+
+  return inputData;
+}
+
 
--- src/sections/@dashboard/invoice/list/index.js
+++ src/sections/@dashboard/invoice/list/index.js
@@ -2,5 +2,7 @@ export { default as InvoiceTableRow } from './InvoiceTableRow';
 export { default as InvoiceAmountTableRow } from './InvoiceAmountTableRow';
 export { default as InvoiceWithdrawTableRow } from './InvoiceWithdrawTableRow';
 export { default as InvoiceDepositTableRow } from './InvoiceDepositTableRow';
+export { default as InvoiceAdminTableRow } from './InvoiceAdminTableRow';
 export { default as InvoiceTableToolbar } from './InvoiceTableToolbar';
 export { default as InvoiceAmountTableToolbar } from './InvoiceAmountTableToolbar';
+export { default as InvoiceAdminTableToolbar } from './InvoiceAdminTableToolbar';
